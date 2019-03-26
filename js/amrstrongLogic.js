a449fa1a023f8ecbf2e0f69db9cea826f6813a7f
david damilola <daviddamilola20@gmail.com>
2019-03-26 13:00:53 -0700

    armstrong app that checks if a number is an armstrong number

---
+++ js/amrstrongLogic.js
@@ -0,0 +1,112 @@
+// const numInput = document.querySelector("#numInput");
+// const btn = document.getElementById("btn");
+// const result = document.querySelector(".result");
+// const checkArmIfStrong = numInput => {
+//   let inputCache = numInput.value;
+//   let arrayOfIndividualValues = [];
+//   //populate the arrayOfIndividualValues with each number of the input value
+//   for (let i in inputCache) {
+//     arrayOfIndividualValues.push(inputCache[i]);
+//   }
+//   //make a new array of the cube of each item in arrayOfIndividualValues
+//   let armTest = arrayOfIndividualValues.map(x => {
+//     return x ** 3;
+//   });
+//   //get the sum
+//   let sumArmTest = armTest.reduce((a, b) => {
+//     return a + b;
+//   });
+//   let numInputCache = Number(inputCache);
+//   if (sumArmTest === numInputCache) {
+//     return "true";
+//   } else {
+//     return "false";
+//   }
+// };
+// btn.addEventListener(
+//   "click",
+//   function(e) {
+//     e.preventDefault();
+//     const enteredNumber = numInput.value;
+//     if (numInput.value.length == 3 && Number(enteredNumber)) {
+//       if (checkArmIfStrong(numInput) == "true") {
+//         result.innerHTML = `<p> ${Number(
+//           enteredNumber
+//         )} is an ArmStrong Number</p>`;
+//       } else {
+//         result.innerHTML = `<p> ${Number(
+//           enteredNumber
+//         )} is NOT an ArmStrong Number</p>`;
+//       }
+//       numInput.value = "";
+//     } else {
+//       result.innerHTML = `<p><strong>warning:</strong> you need to provide a valid 3 digit number</p>`;
+//       numInput.value = "";
+//     }
+//   },
+//   false
+// );
+const numInput = document.querySelector("#numInput");
+const btn = document.getElementById("btn");
+const result = document.querySelector(".result");
+const checkArmIfStrong = numInput => {
+  let inputCache = numInput.value;
+  let arrayOfIndividualValues = [];
+  //populate the arrayOfIndividualValues with each number of the input value
+  for (let i in inputCache) {
+    arrayOfIndividualValues.push(inputCache[i]);
+  }
+  //make a new array of the cube of each item in arrayOfIndividualValues
+  let armTest = arrayOfIndividualValues.map(x => {
+    return x ** 3;
+  });
+  //get the sum
+  let sumArmTest = armTest.reduce((a, b) => {
+    return a + b;
+  });
+  let numInputCache = Number(inputCache);
+  if (sumArmTest === numInputCache) {
+    return "true";
+  } else {
+    return "false";
+  }
+};
+btn.addEventListener(
+  "mouseover",
+  function (e) {
+    e.preventDefault();
+    const enteredNumber = numInput.value;
+    if (!(numInput.value.length == 3 && Number(enteredNumber))) {
+      btn.setAttribute("disabled", "disabled");
+      result.innerHTML =
+        "<strong>Tip:</strong> Enter a valid three digit number";
+    }
+  },
+  false
+);
+
+numInput.addEventListener(
+  "change",
+  function () {
+    btn.removeAttribute("disabled", "disabled");
+    result.innerHTML = "";
+  },
+  false
+);
+
+btn.addEventListener(
+  "click",
+  function () {
+    const enteredNumber = numInput.value;
+    if (checkArmIfStrong(numInput) == "true") {
+      result.innerHTML = `<p> ${Number(
+        enteredNumber
+      )} is an ArmStrong Number</p>`;
+    } else {
+      result.innerHTML = `<p> ${Number(
+        enteredNumber
+      )} is NOT an ArmStrong Number</p>`;
+    }
+  },
+  false
+);
